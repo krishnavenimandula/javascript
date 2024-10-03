cbc9fe1a2a37b0d54e7e033ce1f3b11967efda2f
krishnavenimandula <krishnavenimandula123@gmail.com>
2024-10-03 11:30:34 +0100

    updated destruction file

--- Basics/destructor1.js
+++ Basics/destructor1.js
@@ -10,9 +10,12 @@ const findWeapons = (color) => {
   color.split("\n").forEach((row) => output.push(row.split(",")));
 
   return output;
-  for (const key of getWeapons) {
-    const [id, weapon, cost] = key.split(",");
-    console.log(`${weapon} costs $${cost}`);
-  }
 };
-findWeapons(weapons);
+
+const getWeapons = (findWeapons) => {
+  findWeapons.forEach((element) => {
+    console.log(element[1] + " cost " + element[2]);
+  });
+};
+
+console.log(getWeapons(findWeapons(weapons)));
